e06c1fbfd75242771bea3661f62f2ba79db72ca3
Adil Ahnaf <adilahnaf151@gmail.com>
2026-08-15 17:01:15 +0600

    Add project files.

---
+++ DataAccessLayer/SqlDb/DbConnectionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.SqlDb
+{
+    internal class DbConnectionInfo
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
