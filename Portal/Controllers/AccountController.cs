c933481c4ec2f2c92cc09533ae348627b43c22ef
Adil Ahnaf <adilahnaf151@gmail.com>
2026-08-16 13:17:20 +0600

    Error fixed.

--- Portal/Controllers/AccountController.cs
+++ Portal/Controllers/AccountController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Portal.ViewModels;
-using Newtonsoft.Json;
 
 namespace Portal.Controllers
 {
